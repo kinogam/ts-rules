4d976978c6771ed6faadf32f2535d4107e10c1a8
张磊 <zhanglei@itjuzi.com>
2017-02-13 17:56:07 +0800

    delete files

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-declare function rules(config: Object, message?: Object): (origin: any) => {
-    invalid: boolean;
-};
-export { rules };
--- index.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", './src/rules'], function (require, exports, rules_1) {
-    "use strict";
-    exports.rules = rules_1.rules;
-});
-//# sourceMappingURL=index.js.map
